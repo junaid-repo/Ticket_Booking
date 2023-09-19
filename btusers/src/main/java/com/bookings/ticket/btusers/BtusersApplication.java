2ef290390a7e5b5f29e73efae00fbd6af336b192
junaid-repo <junaidraza3001@gmail.com>
2023-09-19 15:09:53 +0530

    Jwt Impl

--- btusers/src/main/java/com/bookings/ticket/btusers/BtusersApplication.java
+++ btusers/src/main/java/com/bookings/ticket/btusers/BtusersApplication.java
@@ -2,8 +2,10 @@ package com.bookings.ticket.btusers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BtusersApplication {
 
 	public static void main(String[] args) {
--- btusers/src/main/java/com/bookings/ticket/btusers/controller/BTUsersController.java
+++ btusers/src/main/java/com/bookings/ticket/btusers/controller/BTUsersController.java
@@ -3,6 +3,9 @@ package com.bookings.ticket.btusers.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,28 +21,38 @@ import com.bookings.ticket.btusers.service.UserService;
 @RestController
 @RequestMapping("/bt/users")
 public class BTUsersController {
-	
+
 	@Autowired
 	UserService serv;
-	
+
+	@Autowired
+	AuthenticationManager authenticationManager;
+
 	@PostMapping("/auth/register")
-	ResponseEntity<BaseOutput> regsiterUser(@RequestBody UserCredential req){
-		
+	ResponseEntity<BaseOutput> regsiterUser(@RequestBody UserCredential req) {
+
 		BaseOutput response = new BaseOutput();
-		response=serv.registerUser(req);
-		
+		response = serv.registerUser(req);
+
 		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 	}
-	
+
 	@PostMapping("/auth/generateToken")
-	ResponseEntity<String> generateToken(@RequestBody AuthRequest au){
-		String response="";
-		response=serv.generateToken(au.getUsername());
-		
+	ResponseEntity<String> generateToken(@RequestBody AuthRequest au) {
+		String response = "";
+		Authentication authenticate = authenticationManager
+				.authenticate(new UsernamePasswordAuthenticationToken(au.getUsername(), au.getPassword()));
+		if (authenticate.isAuthenticated()) {
+			response = serv.generateToken(au.getUsername());
+		} else {
+			throw new RuntimeException("Invalid access");
+		}
+
 		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 	}
+
 	@GetMapping("/auth/validateToken/{token}")
-	ResponseEntity<String> validateToken(@PathVariable String token){
+	ResponseEntity<String> validateToken(@PathVariable String token) {
 		serv.validateToken(token);
 		return ResponseEntity.status(HttpStatus.FOUND).body("Token is valid");
 	}
